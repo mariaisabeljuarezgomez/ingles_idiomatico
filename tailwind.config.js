9736d2713d98b6a88d011dd3ea77195bac8eeb5e
mariaisabeljuarezgomez <mariaisabeljuarezgomez85@gmail.com>
2025-05-20 22:31:30 -0600

    Add Tailwind CSS configuration and update login page UI

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4F46E5",
+        secondary: "#6366F1"
+      },
+      animation: {
+        'blob': 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.1)',
+          },
+          '66%': {
+            transform: 'translate(-20px, 20px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        }
+      }
+    },
+  },
+  plugins: [],
+} 
